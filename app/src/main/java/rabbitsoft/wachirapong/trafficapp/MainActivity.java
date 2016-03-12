00c72708a4093afffad0aaacd8c3512fefef17cb
Wachirapong <wachirapong.kamutchat@gmail.com>
2016-03-12 15:00:05 +0700

    Finished March_2016_12

--- app/src/main/java/rabbitsoft/wachirapong/trafficapp/MainActivity.java
+++ app/src/main/java/rabbitsoft/wachirapong/trafficapp/MainActivity.java
@@ -1,6 +1,8 @@
 package rabbitsoft.wachirapong.trafficapp;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -41,6 +43,12 @@ public class MainActivity extends AppCompatActivity {
                 //Sound Effect
                 MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.bird_maxican);
                 mediaPlayer.start();
+
+
+                //WebView
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/oC5d3Eo0TL8"));
+                startActivity(intent);
 
             }   //OnClck
         });

b2800df547837479edbfca4fd1174ecd6b8e4c2d
Wachirapong <wachirapong.kamutchat@gmail.com>
2016-03-13 14:47:49 +0700

    Show View Success Full

--- app/src/main/java/rabbitsoft/wachirapong/trafficapp/DetailActivity.java
+++ app/src/main/java/rabbitsoft/wachirapong/trafficapp/DetailActivity.java
@@ -2,12 +2,47 @@ package rabbitsoft.wachirapong.trafficapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView trafficImageView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-}
+
+        bindWidget();
+        showView();
+    }   //Main Method
+
+    private void showView() {
+
+        String strTitle = getIntent().getStringExtra("Title");
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        int inDex = getIntent().getIntExtra("Index",0);
+
+        titleTextView.setText(strTitle);
+        trafficImageView.setImageResource(intIcon);
+
+        String[] detailStrings = getResources().getStringArray(R.array.detail_long);
+        detailTextView.setText(detailStrings[inDex]);
+
+
+    }   //ShowSelectedData New View
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        trafficImageView = (ImageView) findViewById(R.id.imageView2);
+
+    }   // BindWedget
+
+}   // Main Class
--- app/src/main/java/rabbitsoft/wachirapong/trafficapp/MainActivity.java
+++ app/src/main/java/rabbitsoft/wachirapong/trafficapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void createListView() {
 
-        int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
+        final int[] intIcon = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03,
                 R.drawable.traffic_04, R.drawable.traffic_05, R.drawable.traffic_06,
                 R.drawable.traffic_07, R.drawable.traffic_08, R.drawable.traffic_09,
                 R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
@@ -47,28 +48,28 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.traffic_19, R.drawable.traffic_20};
 
 
-        String[] titleStrings = new String[20];
+        final String[] titleStrings = new String[20];
 
-        titleStrings[0]= "หัวข้อหลักที่ 1";
-        titleStrings[1]= "หัวข้อหลักที่ 2";
-        titleStrings[2]= "หัวข้อหลักที่ 3";
-        titleStrings[3]= "หัวข้อหลักที่ 4";
-        titleStrings[4]= "หัวข้อหลักที่ 5";
-        titleStrings[5]= "หัวข้อหลักที่ 6";
-        titleStrings[6]= "หัวข้อหลักที่ 7";
-        titleStrings[7]= "หัวข้อหลักที่ 8";
-        titleStrings[8]= "หัวข้อหลักที่ 9";
-        titleStrings[9]= "หัวข้อหลักที่ 10";
-        titleStrings[10]= "หัวข้อหลักที่ 11";
-        titleStrings[11]= "หัวข้อหลักที่ 12";
-        titleStrings[12]= "หัวข้อหลักที่ 13";
-        titleStrings[13]= "หัวข้อหลักที่ 14";
-        titleStrings[14]= "หัวข้อหลักที่ 15";
-        titleStrings[15]= "หัวข้อหลักที่ 16";
-        titleStrings[16]= "หัวข้อหลักที่ 17";
-        titleStrings[17]= "หัวข้อหลักที่ 18";
-        titleStrings[18]= "หัวข้อหลักที่ 19";
-        titleStrings[19]= "หัวข้อหลักที่ 20";
+        titleStrings[0]= "ห้ามเลี้ยวซ้าย";
+        titleStrings[1]= "ห้ามเลี้ยวขวา";
+        titleStrings[2]= "ตรงไป";
+        titleStrings[3]= "บังคับเลี้ยวขวา";
+        titleStrings[4]= "บังคับเลี้ยวซ้าย";
+        titleStrings[5]= "ทางออก";
+        titleStrings[6]= "ทางเข้า";
+        titleStrings[7]= "ทางออก";
+        titleStrings[8]= "หยุด";
+        titleStrings[9]= "ความสูงไม่เกิน 2.50 เมตร";
+        titleStrings[10]= "ทางแยก";
+        titleStrings[11]= "ห้ามกลับรถ";
+        titleStrings[12]= "ห้ามหยุดรถ";
+        titleStrings[13]= "มีรถสวนทาง";
+        titleStrings[14]= "ห้ามแซง";
+        titleStrings[15]= "ทางเข้า";
+        titleStrings[16]= "หยุดตรวจ";
+        titleStrings[17]= "จำกัดความเร็ว";
+        titleStrings[18]= "กำหนดความกว้าง";
+        titleStrings[19]= "กำหนดความสูง";
 
 
         String[] detailStrings = getResources().getStringArray(R.array.detail_shot);
@@ -80,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        traffic_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+
+                intent.putExtra("Title",titleStrings[i]);
+                intent.putExtra("Image", intIcon[i]);
+                intent.putExtra("Index",i);
+                startActivity(intent);
+
+
+
+            } // On ItemClick
+        });
 
 
     }   //Create ListView
@@ -93,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 //Sound Effect
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.bird_maxican);
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.attack);
                 mediaPlayer.start();
 
 
